259994f5385d3635ea8d1739d3d58d6d9001b7a2
Joshua <jwaiswa7@gmail.com>
2022-01-21 07:33:43 -0600

    Feature/user access (#3)
    * Tests for the user model
    * Tests for the user organization model
    * Added routes /account which is used to get the user's account details. 
    
    # Authenitcation headers
    * token-type
    * access-token
    * client
    * expiary
    * uid
    
    # example response. 
    ```
    {
        "data": {
            "id": "1",
            "type": "user",
            "attributes": {
                "id": 1,
                "email": "test@email.com",
                "organizations": [
                    {
                        "id": 4,
                        "name": "Nienow-Douglas",
                        "created_at": "2022-01-21T12:49:03.839Z",
                        "updated_at": "2022-01-21T12:49:03.839Z"
                    },
                    {
                        "id": 5,
                        "name": "Lindgren-O'Connell",
                        "created_at": "2022-01-21T12:50:28.784Z",
                        "updated_at": "2022-01-21T12:50:28.784Z"
                    }
                ]
            }
        }
    }
    ```

--- spec/models/user_organization_spec.rb
+++ spec/models/user_organization_spec.rb
@@ -3,5 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe UserOrganization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:organization) }
 end
